44a9ff6aff5ac184824de6e84cf0d9a22068890c
fabriciofsousa <134175378+fabriciofsousa@users.noreply.github.com>
2025-04-21 11:45:14 -0300

    feat: teste funcionando com o ambiente de prod

--- adapters/repository/s3_repository.py
+++ adapters/repository/s3_repository.py
@@ -52,7 +52,7 @@ class S3Repository:
                 # Diretório do usuário
                 user_directory = f"{video.user_id}/"
                 video_directory = f"{user_directory}{video.file_name}/"
-                file_key = f"{video_directory}{uuid.uuid4()}_{video.file_name}"
+                file_key = f"{video_directory}_{video.file_name}"
 
                 # Verifica se o vídeo já existe
                 response = await s3.list_objects_v2(Bucket=self.bucket_name, Prefix=video_directory)
